aadd43130e32a78f110e0a07d2c6e77404add95e
ldmotta <ldmotta@visie.com.br>
2011-06-27 17:31:23 -0300

    Adicionando o logo do cobredireto Fazendo funcionar com a versão mais nova do prestashop

---
+++ README.txt
@@ -0,0 +1,54 @@
+﻿=== Módulo de integração CobreDireto + Prestashop ===
+Contributors: 
+    ldmotta(visie.com.br - Implementação do múdulo da visie),
+
+Tags: cobredireto, prestashop
+Module version: 1.0.0
+Tested up to: Prestashop v 1.3.0
+Requires at least: 1.0.0
+Stable tag: 1.3.0
+
+Módulo de integração do Prestashop com o CobreDireto
+
+== Description ==
+
+Permite que o Prestashop utilize o meio de pagamento CobreDireto, de forma fácil e intuitiva, contém todas as
+ferramentas necessárias a esta integração.
+
+
+Algumas notas sobre as seções acima:
+
+*   "Contributors" Lista de contribuidores para construção do módulo separados por vírgula
+*   "Tags" É uma lista separada por vírgulas de tags que se aplicam ao plugin
+*   "Requires at least" É a menor versão do plugin que irá trabalhar em
+*   "Tested up to" É a versão mais alta do e-commerce utilizado com sucesso para testar o plugin *. Note-se que ele pode trabalhar em
+versões superiores ... Este é apenas um mais alto que foi verificado.
+
+== Installation ==
+
+Passos para instalação
+1. Descompacte o módulo na raiz da sua instalação do PrestaShop.
+2. Acesse a área administrativa do seu site e clique na aba “Modules”.
+3. Na lista de módulos “Payment modules”, localize o grupo "Payments & Geteways", em seguida
+o módulo CobreDireto e clique no botão Install.
+4. Retorne ao módulo CobreDireto na lista de módulos e clique em “Configure”.
+5. Na tela de configuração, digite os dados da sua loja no CobreDireto
+
+== Perguntas Frequentes ==
+
+= Eu posso instalar o meu módulo sem ter conhecimentos de php ou qualquer linguagem de programação? =
+
+Pode, você só precisa ter conhecimentos em transferência de dados via FTP ou SFTP, ter os dados de acesso
+ao servidor onde está hospedado a sua aplicação, e ter um gerenciador de arquivos FTP como o FileZilla
+(http://filezilla-project.org/). Entretanto, recomendamos enfaticamente que procure um técnico da área.
+
+= O módulo não funcionou na minha loja, o que fazer? =
+
+Se já verificou a versão da sua loja virtual e ela e a versão testada com o módulo, e ainda assim não funciona,
+entre em contato com o CobreDireto pelo endereço http://cobredireto.com.br
+
+== Changelog ==
+
+= 1.0.1 =
+* Atualizado a biblioteca para funcionar com a versão 1.4.3 do Prestashop.
+
--- modules/cobredireto/cobredireto.php
+++ modules/cobredireto/cobredireto.php
@@ -1,130 +1,131 @@
 <?php
 
 if (!defined('_CAN_LOAD_FILES_'))
-	exit;
+    exit;
 
-class cobredireto extends PaymentModule
-{
+class cobredireto extends PaymentModule {
     private	$_html = '';
-	  private $_postErrors = array();
+    private $_postErrors = array();
     private $all_formas = array (
-            array(
-                'title'                 => 'Cartões de crédito',
-                'visa3dc'               => 'Visa VBV',
-                'redecard_mastercard'   => 'Mastercard Komerci',
-                'redecard_diners'       => 'Diners Komerci',
-                'amex_webpos2p'         => 'Amex WebPOS 2P',
-                'redecard_visa'         => 'Komerci Visa',
-                'redecard_ws_visa'      => 'Komerci WS Visa',
-                'cielo2p_master'        => 'Moset Master',
-                'cielo2p_visa'          => 'Moset Visa',
-                'cielo3p_mastercard'    => 'MASTERCARD VBV',
-                'redecard_ws_mastercard'=> 'Komerci WS MASTERCARD',
-                'redecard_ws_diners'    => 'Komerci WS Diners',
-                'setef_hipercard'       => 'Hipercard Setef',
-            ),
-            array(
-                'title'                 => 'Débito/transferência online',
-                'bradesco'              => 'Bradesco',
-                'itau'                  => 'Itaú',
-                'bb'                    => 'Banco do Brasil',
-                'unibanco'              => 'Unibanco',
-                'real'                  => 'Real',
-                'banrisul_pgta'         => 'Banrisul'
-            ),
-            array (
-                'title'                 => 'Boleto bancário',
-                'boleto_bradesco'       => 'Bradesco',
-                'boleto_itau'           => 'Itaú',
-                'boleto_bb'             => 'Banco do Brasil',
-                'boleto_unibanco'       => 'Unibanco',
-                'boleto_real'           => 'Real',
-            )
-        );
-
+        array(
+            'title'                 => 'Cartões de crédito',
+            'visa3dc'               => 'Visa VBV',
+            'redecard_mastercard'   => 'Mastercard Komerci',
+            'redecard_diners'       => 'Diners Komerci',
+            'amex_webpos2p'         => 'Amex WebPOS 2P',
+            'redecard_visa'         => 'Komerci Visa',
+            'redecard_ws_visa'      => 'Komerci WS Visa',
+            'cielo2p_master'        => 'Moset Master',
+            'cielo2p_visa'          => 'Moset Visa',
+            'cielo3p_mastercard'    => 'MASTERCARD VBV',
+            'redecard_ws_mastercard'=> 'Komerci WS MASTERCARD',
+            'redecard_ws_diners'    => 'Komerci WS Diners',
+            'setef_hipercard'       => 'Hipercard Setef',
+        ),
+        array(
+            'title'                 => 'Débito/transferência online',
+            'bradesco'              => 'Bradesco',
+            'itau'                  => 'Itaú',
+            'bb'                    => 'Banco do Brasil',
+            'unibanco'              => 'Unibanco',
+            'real'                  => 'Real',
+            'banrisul_pgta'         => 'Banrisul'
+        ),
+        array (
+            'title'                 => 'Boleto bancário',
+            'boleto_bradesco'       => 'Bradesco',
+            'boleto_itau'           => 'Itaú',
+            'boleto_bb'             => 'Banco do Brasil',
+            'boleto_unibanco'       => 'Unibanco',
+            'boleto_real'           => 'Real',
+        )
+    );
+    
     public $formas;
-
-	  public function __construct(){
-		    $this->name = 'cobredireto';
-		    $this->tab = 'Payment';
-		    $this->version = '0.1';
-		    
-		    $this->currencies = true;
-		    $this->currencies_mode = 'radio';
-
+    
+    public function __construct() {	  
+        $this->name = 'cobredireto';
+        $this->tab = 'payments_gateways';
+        $this->version = '1.0.1';
+        
+        $this->currencies = true;
+        $this->currencies_mode = 'radio';
+        
         parent::__construct();
-
-		    $this->page = basename(__FILE__, '.php');
+        
+        $this->page = basename(__FILE__, '.php');
         $this->displayName = $this->l('CobreDireto');
         $this->description = $this->l('Aceitar pagamentos com o CobreDireto');
-		    $this->confirmUninstall = $this->l('Você tem certeza que deseja deletar os seus dados ?');
-		    if (trim(Configuration::get('CD_COD_LOJA')) == '' || trim(Configuration::get('CD_COD_USER')) == '' || trim(Configuration::get('CD_PASSWORD')) == '')
-		    	$this->warning = $this->l('Você precisa terminar de configurar os seus dados no CobreDireto');
+        $this->confirmUninstall = $this->l('Você tem certeza que deseja deletar os seus dados ?');
+        if (trim(Configuration::get('CD_COD_LOJA')) == '' || trim(Configuration::get('CD_COD_USER')) == '' || trim(Configuration::get('CD_PASSWORD')) == '')
+            $this->warning = $this->l('Você precisa concluir a configuração dos seus dados do CobreDireto');
         
-        foreach($this->all_formas as $tipos)
-            foreach($tipos as $k=>$v)
+        foreach($this->all_formas as $tipos) {
+            foreach($tipos as $k=>$v) {
                 $this->formas[] = $k;
-	  }
-
-	  public function install(){
-		    if (!parent::install()
-			    OR !Configuration::updateValue('CD_COD_LOJA', '')
-			    OR !Configuration::updateValue('CD_COD_USER', '')
-			    OR !Configuration::updateValue('CD_PASSWORD', '')
-			    OR !Configuration::updateValue('CD_AMBIENTE', 1)
-			    OR !Configuration::updateValue('CD_FORMAS', '[]')
-			    OR !$this->registerHook('payment')
-			    OR !$this->registerHook('paymentReturn'))
-			      return false;
-		    return true;
-	  }
-
-	  public function uninstall(){
-		    if ( !Configuration::deleteByName('CD_COD_LOJA')
-			    OR !Configuration::deleteByName('CD_COD_USER')
-			    OR !Configuration::deleteByName('CD_PASSWORD')
-			    OR !Configuration::deleteByName('CD_AMBIENTE')
-			    OR !Configuration::deleteByName('CD_FORMAS')
-			    OR !parent::uninstall())
-			      return false;
-		    return true;
-	  }
-
-	  public function getContent(){
-		    $this->_html = '<h2>CobreDireto</h2>';
-		    if (isset($_POST['submitCobreDireto'])){
-			      if (empty($_POST['cod_loja']))
+            }
+        }
+    }
+    
+    public function install() {
+        if (!parent::install()
+                OR !Configuration::updateValue('CD_COD_LOJA', '')
+                OR !Configuration::updateValue('CD_COD_USER', '')
+                OR !Configuration::updateValue('CD_PASSWORD', '')
+                OR !Configuration::updateValue('CD_AMBIENTE', 1)
+                OR !Configuration::updateValue('CD_FORMAS', '[]')
+                OR !$this->registerHook('payment')
+                OR !$this->registerHook('paymentReturn'))
+            return false;
+        return true;
+    }
+    
+    public function uninstall() {
+        if ( !Configuration::deleteByName('CD_COD_LOJA')
+                OR !Configuration::deleteByName('CD_COD_USER')
+                OR !Configuration::deleteByName('CD_PASSWORD')
+                OR !Configuration::deleteByName('CD_AMBIENTE')
+                OR !Configuration::deleteByName('CD_FORMAS')
+                OR !parent::uninstall())
+            return false;
+        return true;
+    }
+    
+    public function getContent() {
+        $this->_html = '<h2>CobreDireto</h2>';
+        if (isset($_POST['submitCobreDireto'])) {
+            if (empty($_POST['cod_loja']))
                 $this->_postErrors[] = $this->l('Código da loja obrigatório');
-			      if (empty($_POST['cod_user']))
+            if (empty($_POST['cod_user']))
                 $this->_postErrors[] = $this->l('Usuário obrigatório');
-			      if (empty($_POST['password']))
+            if (empty($_POST['password']))
                 $this->_postErrors[] = $this->l('Senha obrigatório');
-			      if (!sizeof($this->_postErrors)) {
+            if (!sizeof($this->_postErrors)) {
                 Configuration::updateValue('CD_COD_LOJA', strval($_POST['cod_loja']));
                 Configuration::updateValue('CD_COD_USER', strval($_POST['cod_user']));
                 Configuration::updateValue('CD_PASSWORD', strval($_POST['password']));
                 Configuration::updateValue('CD_AMBIENTE', strval($_POST['ambiente']));
                 $this->displayConf();
-			      }
-			      else
+            }
+            else
                 $this->displayErrors();
         }
-        elseif (isset($_POST['submitCobreDiretoFormas'])){
+        elseif (isset($_POST['submitCobreDiretoFormas'])) {
             Configuration::updateValue('CD_FORMAS', json_encode($_POST['formas_pgto']));
         }
-		    $this->displayFormSettings();
-		    return $this->_html;
-	  }
-
-	  public function displayConf(){
+        $this->displayFormSettings();
+        return $this->_html;
+    }
+    
+    public function displayConf() {
         $this->_html .= '
             <div class="conf confirm">
             <img src="../img/admin/ok.gif" alt="'.$this->l('Confirmation').'" />
             '.$this->l('Configurações atualizadas com sucesso').'
             </div>';
-	  }
-
-	  public function displayErrors(){
+    }
+    
+    public function displayErrors() {
         $nbErrors = sizeof($this->_postErrors);
         $this->_html .= '
             <div class="alert error">
@@ -135,17 +136,16 @@ class cobredireto extends PaymentModule
         $this->_html .= '
             </ol>
             </div>';
-	  }
-	
-	
-	  public function displayFormSettings()
-	  {
+    }
+    
+    
+    public function displayFormSettings() {
         $conf = Configuration::getMultiple(array('CD_COD_LOJA', 'CD_COD_USER', 'CD_PASSWORD', 'CD_AMBIENTE'));
-		    $cod_loja = array_key_exists('cod_loja', $_POST) ? $_POST['cod_loja'] : (array_key_exists('CD_COD_LOJA', $conf) ? $conf['CD_COD_LOJA'] : '');
-		    $cod_user = array_key_exists('cod_user', $_POST) ? $_POST['cod_user'] : (array_key_exists('CD_COD_USER', $conf) ? $conf['CD_COD_USER'] : '');
-		    $password = array_key_exists('password', $_POST) ? $_POST['password'] : (array_key_exists('CD_PASSWORD', $conf) ? $conf['CD_PASSWORD'] : '');
-		    $ambiente = array_key_exists('ambiente', $_POST) ? $_POST['ambiente'] : (array_key_exists('CD_AMBIENTE', $conf) ? $conf['CD_AMBIENTE'] : '');
-
+        $cod_loja = array_key_exists('cod_loja', $_POST) ? $_POST['cod_loja'] : (array_key_exists('CD_COD_LOJA', $conf) ? $conf['CD_COD_LOJA'] : '');
+        $cod_user = array_key_exists('cod_user', $_POST) ? $_POST['cod_user'] : (array_key_exists('CD_COD_USER', $conf) ? $conf['CD_COD_USER'] : '');
+        $password = array_key_exists('password', $_POST) ? $_POST['password'] : (array_key_exists('CD_PASSWORD', $conf) ? $conf['CD_PASSWORD'] : '');
+        $ambiente = array_key_exists('ambiente', $_POST) ? $_POST['ambiente'] : (array_key_exists('CD_AMBIENTE', $conf) ? $conf['CD_AMBIENTE'] : '');
+        
         $this->_html .= '
             <fieldset>
                 <legend>'.$this->l('Pre-requisitos').'</legend>
@@ -187,15 +187,15 @@ class cobredireto extends PaymentModule
 		        <div style="clear:both;">
             <br />
         ';
-	  }
-
+    }
+    
     private function checkSoap() { 
         if (class_exists('SoapClient'))
             return '<p class="ok" style="padding-left: 20px;">Classe SOAP built-in</p>';
         else
             return '<p class="fail" style="padding-left: 20px;">Classe SOAP built-in</p>';
     }
-
+    
     private function checkOpenSSL() { 
         require_once(dirname(__FILE__).'/pagamento.php');
         $modules = new moduleCheck;
@@ -204,23 +204,23 @@ class cobredireto extends PaymentModule
         else
             return '<p class="fail" style="padding-left: 20px;">Módulo OpenSSL</p>';
     }
-
+    
     private function checkURL() { 
         if (intval(ini_get('allow_url_fopen')))
             return '<p class="ok" style="padding-left: 20px;">Acesso a URL Externa</p>';
         else
             return '<p class="fail" style="padding-left: 20px;">Acesso a URL Externa</p>';
     }
-
+    
     public function _montaFormas() { 
         $salvas = json_decode(Configuration::get('CD_FORMAS'));
         if (!$salvas)
             $salvas = array();
         $h = '<select name="formas_pgto[]" class="cobredireto_formas" multiple="multiple" style="height: 380px; margin-left: 120px;">';
-        foreach($this->all_formas as $key=>$tipos){
+        foreach($this->all_formas as $key=>$tipos) {
             $h .= '<optgroup label="'.$tipos['title'].'">';
             unset($tipos['title']);
-            foreach($tipos as $k=>$v){
+            foreach($tipos as $k=>$v) {
                 $s = '';
                 if (in_array($k, $salvas))
                     $s = ' selected="true"';
@@ -231,43 +231,41 @@ class cobredireto extends PaymentModule
         $h .= '</select>';
         return $h;
     }
-
-	  public function hookPayment($params)
-	  {
+    
+    public function hookPayment($params) {
         global $smarty;
         if (!$this->active || Configuration::get('CD_COD_LOJA') == '')
             return ;
-
+        
         $escolhidas = json_decode(Configuration::get('CD_FORMAS'));
         if (!$escolhidas)
             $escolhidas = array();
         $tipos = $this->all_formas;
-        foreach($tipos as $t=>$sec){
-            foreach($sec as $k=>$v){
+        foreach($tipos as $t=>$sec) {
+            foreach($sec as $k=>$v) {
                 if (!in_array($k, $escolhidas) && ($k != 'title'))
                     unset($tipos[$t][$k]);
             }
             if (count($tipos[$t]) == 1)
                 unset($tipos[$t]);
         }
-
-		    $smarty->assign('tipos', $tipos);
+        
+        $smarty->assign('tipos', $tipos);
         return $this->display(__FILE__, 'cobredireto.tpl');
-	  }
-
-	  public function hookPaymentReturn($params)
-	  {
+    }
+    
+    public function hookPaymentReturn($params) {
         if (!$this->active)
             return ;
-
+        
         return $this->display(__FILE__, 'confirmation.tpl');
-	  }
-
-	  function validateOrder($id_cart, $id_order_state, $amountPaid, $paymentMethod = 'Unknown', $message = NULL, $extraVars = array(), $currency_special = NULL, $dont_touch_amount = false)
-	  {
+    }
+    
+    function validateOrder($id_cart, $id_order_state, $amountPaid, $paymentMethod = 'Unknown', $message = NULL, $extraVars = array(), $currency_special = NULL, $dont_touch_amount = false) {
         if (!$this->active)
             return ;
-
+        
         parent::validateOrder($id_cart, $id_order_state, $amountPaid, $paymentMethod, $message, $extraVars);
-	  }
+    }
 }
+
